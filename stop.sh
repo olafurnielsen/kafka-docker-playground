b75c022aee64d6becd00ccf266084bbba5c5cf7c
olafurnielsen <69799161+olafurnielsen@users.noreply.github.com>
2021-09-10 12:11:52 +0200

    Initial commit

---
+++ stop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+docker-compose \
+  -f services/docker-compose.zookeeper.yml \
+  -f services/docker-compose.broker.yml \
+  -f services/docker-compose.schema-registry.yml \
+  -f services/docker-compose.kafka-connect.yml \
+  -f services/docker-compose.ksqldb.yml \
+  -f services/docker-compose.control-center.yml \
+  -f services/docker-compose.mysql.yml \
+  -f services/docker-compose.postgres.yml \
+  -f services/docker-compose.elasticsearch.yml \
+  -f services/docker-compose.kibana.yml \
+  -f services/docker-compose.neo4j.yml \
+  down
