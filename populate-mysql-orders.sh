bd3a7368910046eb7a462a8ea278aa912b417b6a
olafurnielsen <69799161+olafurnielsen@users.noreply.github.com>
2021-09-21 01:17:10 +0200

    Local perm storage for services configured

---
+++ populate-mysql-orders.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker exec mysql /data/02_populate_more_orders.sh
--- start.sh
+++ start.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+# Create data directories for service volumes if not present
+# These directories are used for data persistance
+declare -a DATA_DIRS=(
+  './data/service-volumes/.zookeeper-data'
+  './data/service-volumes/.zookeeper-tx-logs'
+  './data/service-volumes/.kafka-data'
+  './data/service-volumes/.mysql-data'
+  './data/service-volumes/.postgres-data'
+)
+
+for DIR in ${DATA_DIRS[*]}
+do
+  if [ ! -d "${DIR}" ]
+  then
+    echo "Creating ${DIR}"
+    mkdir -p $DIR
+    chown -R 1000:1000 $DIR
+  fi
+done
+
 docker-compose \
   -f services/docker-compose.zookeeper.yml \
   -f services/docker-compose.broker.yml \
